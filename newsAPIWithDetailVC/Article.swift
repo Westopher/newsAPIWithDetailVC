07c39a56e2de3ff1584b287dc83d69a179e18feb
West Kraemer <kraemerwest@gmail.com>
2019-08-07 14:33:44 +0200

    set up model for decoding json for articles from api

---
+++ newsAPIWithDetailVC/Article.swift
@@ -0,0 +1,25 @@
+//
+//  Article.swift
+//  newsAPIWithDetailVC
+//
+//  Created by West Kraemer on 8/7/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleService: Decodable {
+    var totalResults: Int?
+    var articles: [Article]?
+}
+
+
+struct Article: Decodable {
+    var author: String?
+    var title: String?
+    var description: String?
+    var url: String?
+    var urlToImage: String?
+    var publishedAt: String?
+    
+}

bd8389974e0eb7ec5df8f1265323a82f6096194f
West Kraemer <kraemerwest@gmail.com>
2019-08-08 08:25:53 +0200

    add article model with get articles func

---
+++ newsAPIWithDetailVC/Model/ArticleModel.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleModel.swift
+//  newsAPIWithDetailVC
+//
+//  Created by West Kraemer on 8/8/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import Foundation
+
+class ArticleModel {
+    func getArticles() {
+        
+    
+    }
+}

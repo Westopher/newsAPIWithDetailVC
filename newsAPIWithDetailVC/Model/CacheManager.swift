1cc559b4b890e1844372fa08d00a316e656ade60
West Kraemer <kraemerwest@gmail.com>
2019-08-11 13:58:52 +0200

    add cache manager file and begin cache of data with static funcs and var

---
+++ newsAPIWithDetailVC/Model/CacheManager.swift
@@ -0,0 +1,25 @@
+//
+//  CacheManager.swift
+//  newsAPIWithDetailVC
+//
+//  Created by West Kraemer on 8/11/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import Foundation
+
+class CacheManager {
+    
+    static var imageDictionary = [String:Data]()
+    
+    static func saveImageData(url: String, data: Data) {
+        imageDictionary[url] = data
+        
+    }
+    
+    
+    static func retrieveImageData(url: String) -> Data? {
+        return imageDictionary[url]
+    }
+    
+}
